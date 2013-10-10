9b9ac63bd283f3e410fdaecc022be12ca82eb82d
papa-pc <papersnail@gmail.com>
2013-10-09 22:47:40 -0700

    Working example

--- lambda_proc.rb
+++ lambda_proc.rb
@@ -1,3 +1,21 @@
 class LambdaProc
-  # To change this template use File | Settings | File Templates.
+  def do_some_proc
+    proc_sample(1, "Hello") do | arg1, arg2 |
+      p "Inside do_some_proc"
+      p arg1
+      p arg2
+    end
+
+  end
+
+  def proc_sample(param1, param2, &block)
+    p "Inside proc_sample"
+    arg1 = param1 + 1
+    arg2 = "#{param2} world"
+    yield arg1, arg2
+  end
 end
+
+
+lambda_proc = LambdaProc.new
+lambda_proc.do_some_proc
