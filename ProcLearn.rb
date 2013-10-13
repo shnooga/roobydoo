2285817ba1db08fc443fa0b9ad34f60fffd2dc89
papa-pc <papersnail@gmail.com>
2013-10-13 14:39:11 -0700

    Begin proc example

--- ProcLearn.rb
+++ ProcLearn.rb
@@ -1,20 +1,36 @@
 class ProcLearn
-  def do_some_proc
-    proc_sample(1, "Hello") do | arg1, arg2 |
-      p "  Inside do_some_proc"
+  def do_some_block
+    block_demo(1, "Hello") do | arg1, arg2 |
+      p "  Inside do_some_block"
       p "    #{arg1}  #{arg2} "
     end
+  end
+
+  def do_some_proc
+    my_proc = Proc.new() do |param1, param2|
+      p " Inside of Proc"
+      #p arg1 = param1 * 10
+      #p arg2 = param2.reverse!
+      #p param1 * 10
+      #p param2.reverse!
+    end
+    proc_demo(my_proc)
 
   end
 
-  def proc_sample(param1, param2, &block)
-    p "Inside proc_sample"
+  private
+  def block_demo(param1, param2, &block)
+    p "Inside block_demo"
     arg1 = param1 + 1
     arg2 = "#{param2} world"
     yield arg1, arg2
   end
+
+  def proc_demo(some_proc)
+    p "Inside proc_demo"
+    some_proc.call()
+  end
 end
 
-
-my_proc = ProcLearn.new
-my_proc.do_some_proc
+ProcLearn.new.do_some_block
+ProcLearn.new.do_some_proc
