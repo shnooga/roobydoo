70288021392f8c3db59addefdc89463b128288b0
Huy Nguyen <hnguyen@streamsend.com>
2014-01-10 14:24:07 -0800

    Added lambda code

--- ProcLearn.rb
+++ ProcLearn.rb
@@ -5,38 +5,56 @@ class ProcLearn
 
   # Blocks are for one time usage
   def do_some_block
-    block_demo(UNO, HELLO) do | arg1, arg2 |
+    block_demo(UNO, HELLO) do | out_arg1, out_arg2 |
       p "  Inside do_some_block"
-      p "    #{arg1}  #{arg2} "
+      p "    #{out_arg1}  #{out_arg2} "
     end
   end
 
   # Procs allow code reuse
   def do_some_proc
-    my_proc = Proc.new() do |arg1, arg2|
+    my_proc = Proc.new() do |out_arg1, out_arg2|
       p " Inside of Proc"
-      p "    #{arg1}  #{arg2} "
+      p "    #{out_arg1}  #{out_arg2} "
     end
 
     proc_demo(UNO, HELLO, my_proc) # Call 1st time
     proc_demo(UNO, WORLD, my_proc) # Call 2nd time
   end
 
+  def do_some_lambda
+    my_proc = Proc.new() do |out_arg1, out_arg2|
+      p " Inside of Proc"
+      p "    #{out_arg1}  #{out_arg2} "
+    end
+
+    lambda_demo(UNO, HELLO, my_proc) # Call 1st time
+    lambda_demo(UNO, WORLD, my_proc) # Call 2nd time
+  end
+
   private
   def block_demo(param1, param2, &block)
     p "Inside block_demo"
-    arg1 = param1 + 1
-    arg2 = "#{param2} world"
-    yield arg1, arg2
+    out_arg1 = param1 + 1
+    out_arg2 = "#{param2} world"
+    yield out_arg1, out_arg2
   end
 
   def proc_demo(param1, param2, some_proc)
     p "Inside proc_demo"
-    arg1 = param1 * 10
-    arg2 = param2.reverse
-    some_proc.call(arg1, arg2)
+    out_arg1 = param1 * 10
+    out_arg2 = param2.reverse
+    some_proc.call(out_arg1, out_arg2)
+  end
+
+  def lambda_demo(param1, param2, some_proc)
+    p "Inside lambda demo"
+    out_arg1 = param1 * 111
+    out_arg2 = param2.upcase
+    some_proc.call(out_arg1, out_arg2)
   end
 end
 
 ProcLearn.new.do_some_block
 ProcLearn.new.do_some_proc
+ProcLearn.new.do_some_lambda
